ebe13f7403cc626e4ca272326c95a3add6d0dacf
Jakson Bernardo <jaksondevweb@gmail.com>
2026-02-27 20:20:23 -0300

    ADICIONANDO SCHEMAS DE AUTENTICAÇÃO

---
+++ car_api/schemas/auth.py
@@ -0,0 +1,26 @@
+from pydantic import BaseModel, EmailStr, field_validator
+
+
+class Token(BaseModel):
+
+    access_token: str
+    token_type: str
+
+
+class LoginRequest(BaseModel):
+
+    email: EmailStr
+    password: str
+
+    @field_validator("password")
+    def password_min_length(cls, v):
+
+        if len(v.strip()) < 6:
+
+            raise ValueError("Password deve ter pelo menos 6 caracteres")
+        
+        return v
+
+
+
+
